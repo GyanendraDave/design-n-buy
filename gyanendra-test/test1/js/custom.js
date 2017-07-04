085107941e3c785e03c332d1a90730b23cbdc6f1
GyanendraDave <gyandreamworld7868@gmail.com>
2017-07-04 14:54:35 +0530

    Add files via upload

---
+++ gyanendra-test/test1/js/custom.js
@@ -0,0 +1,30 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
+
+
+
+
+
+/*Hide-top Header*/
+$(".close-item").click(function(){
+        $(".top-header").hide();
+    });
+
+
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
